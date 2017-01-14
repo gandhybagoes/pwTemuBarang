0ae861112465f41d0b3218c8203ba287ed0b43c9
oyi77 <mbahkoe.pendekar@gmail.com>
2017-01-15 00:09:20 +0700

    add Login Pages
    But still not going anywhere

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
+		function __construct(){
+			parent::__construct();		
+			$this->load->model('modelmu');
+			$this->load->library('session');
+
+		}
+
 	public function index()
 	{
 		$this->load->view('pages/login');
@@ -10,7 +17,46 @@ class Login extends CI_Controller {
 
 	public function kliklogin()
 	{
-		$username = $this->post->username;
-		$password = $this->post->password;
+		if(null !== $this->input->post('lgnbtn')){
+			$username =  $this->input->post('username');
+			$password = $this->input->post('password');
+
+			$data = array(
+				'nama_user' => $username,
+				'password_user' => $password
+				);
+			$q = $this->modelmu->select($data, 'login');
+			if(null !== $q){
+				$this->session->start();
+				$this->session_set_userdata('tipe_user' => $q['type_user']);
+				if($q['type_user'] == 1){
+
+				}
+				else if($q['type_user'] == 0){
+
+				}
+				else {
+					echo "<script>alert('Access Denied!')</script>";
+				}
+			}
+
+		}	
+		else if(null !== $this->input->post('lgnfb'))
+		{
+			echo "iki fb";
+			//api login fb
+		}
+		else if(null !== $this->input->post('lgntwt'))
+		{
+			echo "iki twitter";
+			//api login fb
+		}
+		else if(null !== $this->input->post('fgtbtn')){
+			echo "iki forget pass";
+			//iki ngirim email
+		}
+		else {
+			echo "?????";
+		}
 	}
 }
---
+++ application/models/modelmu.php
@@ -0,0 +1,35 @@
+<?php
+class Modelmu extends CI_Model {
+
+        public $title;
+        public $content;
+        public $date;
+
+        public function select($where, $table)
+        {
+                $query = $this->db->select('*')
+                         ->from($table)
+                         ->where($where)
+                         ->get();
+                return $query->row_array();
+        }
+
+        public function insert_entry()
+        {
+                $this->title    = $_POST['title']; // please read the below note
+                $this->content  = $_POST['content'];
+                $this->date     = time();
+
+                $this->db->insert('entries', $this);
+        }
+
+        public function update_entry()
+        {
+                $this->title    = $_POST['title'];
+                $this->content  = $_POST['content'];
+                $this->date     = time();
+
+                $this->db->update('entries', $this, array('id' => $_POST['id']));
+        }
+
+}
